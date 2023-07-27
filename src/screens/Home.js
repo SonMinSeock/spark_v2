91163e313fd0524a8f13b4c80cab3db3c69fe88a
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-27 17:56:58 +0900

    출석 보상 확인 모달 구현

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,6 +4,7 @@ import { IoMdContact } from "react-icons/io";
 import CoinIcon from "../assets/akar-icons_coin.png";
 import FemaleImage from "../assets/female_image.png";
 import MaleImage from "../assets/male_image.png";
+import RobotSmileImage from "../assets/robot_smile.png";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { dbService } from "../db/firebase";
@@ -15,144 +16,147 @@ import {
   doc,
   updateDoc,
 } from "firebase/firestore";
+import Backdrop from "./UI/Modal/Backdrop";
+import Modal from "./UI/Modal/Modal";
+
+const HomeBackgroundBox = styled.div`
+  height: 100vh;
+  background-color: #e2ebf0;
+  @media (min-width: 40rem) {
+    margin: 0 auto;
+    max-width: 25rem;
+  }
+`;
+const Header = styled.header`
+  background-color: #58c5b0;
+  color: #ffffff;
+  border-bottom-left-radius: 0.8rem;
+  border-bottom-right-radius: 0.8rem;
+`;
+const Top = styled.div`
+  padding: 1rem 1.2rem;
+  display: flex;
+  justify-content: space-between;
+  /* margin-bottom: 1.5rem; */
+  & div {
+    display: flex;
+    align-items: center;
+    & span {
+      font-size: 1.3rem;
+      margin-left: 0.6rem;
+    }
+  }
+  & svg {
+    cursor: pointer;
+  }
+`;
+
+const CoinLogo = styled.img`
+  width: 1.3rem;
+  height: 1.3rem;
+`;
+
+const RecomendBox = styled.div`
+  color: white;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+`;
+
+const Text = styled.div`
+  padding: 0 1.2rem;
+  margin-bottom: 1rem;
+  & span {
+    display: block;
+  }
+  & span:first-child {
+    font-weight: bold;
+    font-size: 1.5rem;
+    margin-bottom: 0.5rem;
+  }
+`;
+const RecomendFriends = styled.div`
+  display: flex;
+  padding: 0 1.2rem;
+`;
+const RecomendFriend = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  width: 6.5rem;
+  height: 7.6rem;
+  border-radius: 0.4rem;
+  margin: 0rem 0.5rem;
+  &:first-child {
+    margin-left: 0rem;
+  }
+  &:last-child {
+    margin-right: 0rem;
+  }
+  color: #000;
+  cursor: pointer;
+  &:last-child {
+    margin-right: 0px;
+  }
+  & span {
+    margin-top: 0.4rem;
+    font-weight: bold;
+  }
+`;
+const ProfileImg = styled.img`
+  display: block;
+  width: 3rem;
+  height: 3rem;
+`;
+const Main = styled.main`
+  padding: 0 1.2rem;
+  padding-top: 0.6rem;
+  margin-bottom: 0.4rem;
+  & > span {
+    display: block;
+    color: #000;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+`;
+const TextBox = styled.div`
+  padding: 0 1.2rem;
+  margin-bottom: 2rem;
+`;
+const ChatFriends = styled.div`
+  height: calc(100vh - 22rem);
+  overflow: scroll;
+`;
+
+const ChatFriend = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 0.5rem;
+  margin-bottom: 0.5rem;
+  padding: 0.4rem 0.8rem;
+  cursor: pointer;
+`;
+
+const FriendInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 0.6rem;
+  & span:first-child {
+    font-weight: bold;
+    margin-bottom: 0.3rem;
+  }
+  & span:last-child {
+    color: gray;
+  }
+  font-size: 0.8rem;
+`;
 
 function Home() {
-  const HomeBackgroundBox = styled.div`
-    height: 100vh;
-    background-color: #e2ebf0;
-    @media (min-width: 40rem) {
-      margin: 0 auto;
-      max-width: 25rem;
-    }
-  `;
-  const Header = styled.header`
-    background-color: #58c5b0;
-    color: #ffffff;
-    border-bottom-left-radius: 0.8rem;
-    border-bottom-right-radius: 0.8rem;
-  `;
-  const Top = styled.div`
-    padding: 1rem 1.2rem;
-    display: flex;
-    justify-content: space-between;
-    /* margin-bottom: 1.5rem; */
-    & div {
-      display: flex;
-      align-items: center;
-      & span {
-        font-size: 1.3rem;
-        margin-left: 0.6rem;
-      }
-    }
-    & svg {
-      cursor: pointer;
-    }
-  `;
-
-  const CoinLogo = styled.img`
-    width: 1.3rem;
-    height: 1.3rem;
-  `;
-
-  const RecomendBox = styled.div`
-    color: white;
-    margin-bottom: 1rem;
-    padding-bottom: 1rem;
-  `;
-
-  const Text = styled.div`
-    padding: 0 1.2rem;
-    margin-bottom: 1rem;
-    & span {
-      display: block;
-    }
-    & span:first-child {
-      font-weight: bold;
-      font-size: 1.5rem;
-      margin-bottom: 0.5rem;
-    }
-  `;
-  const RecomendFriends = styled.div`
-    display: flex;
-    padding: 0 1.2rem;
-  `;
-  const RecomendFriend = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: #ffffff;
-    width: 6.5rem;
-    height: 7.6rem;
-    border-radius: 0.4rem;
-    margin: 0rem 0.5rem;
-    &:first-child {
-      margin-left: 0rem;
-    }
-    &:last-child {
-      margin-right: 0rem;
-    }
-    color: #000;
-    cursor: pointer;
-    &:last-child {
-      margin-right: 0px;
-    }
-    & span {
-      margin-top: 0.4rem;
-      font-weight: bold;
-    }
-  `;
-  const ProfileImg = styled.img`
-    display: block;
-    width: 3rem;
-    height: 3rem;
-  `;
-  const Main = styled.main`
-    padding: 0 1.2rem;
-    padding-top: 0.6rem;
-    margin-bottom: 0.4rem;
-    & > span {
-      display: block;
-      color: #000;
-      font-weight: bold;
-      margin-bottom: 1rem;
-    }
-  `;
-  const TextBox = styled.div`
-    padding: 0 1.2rem;
-    margin-bottom: 2rem;
-  `;
-  const ChatFriends = styled.div`
-    height: calc(100vh - 22rem);
-    overflow: scroll;
-  `;
-
-  const ChatFriend = styled.div`
-    display: flex;
-    align-items: center;
-    background-color: #ffffff;
-    border-radius: 0.5rem;
-    margin-bottom: 0.5rem;
-    padding: 0.4rem 0.8rem;
-    cursor: pointer;
-  `;
-
-  const FriendInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-left: 0.6rem;
-    & span:first-child {
-      font-weight: bold;
-      margin-bottom: 0.3rem;
-    }
-    & span:last-child {
-      color: gray;
-    }
-    font-size: 0.8rem;
-  `;
-
   const [users, setUsers] = useState([]);
+  const [rewordModal, setRewordModal] = useState(false);
 
   const navigate = useNavigate();
   const {
@@ -176,6 +180,7 @@ function Home() {
         currentDate: date,
         dateViewList: [...userInfo.dateViewList, date],
       });
+      setRewordModal(true);
     } else {
       await updateDoc(userRef, {
         currentDate: date,
@@ -197,6 +202,7 @@ function Home() {
     } else {
       if (userInfo.currentDate !== currentDate) {
         updateUser(currentDate, true);
+        // toggleModal();
       }
     }
   };
@@ -261,49 +267,58 @@ function Home() {
     ));
   };
   return (
-    <HomeBackgroundBox>
-      <Header>
-        <Top>
-          <div>
-            <CoinLogo src={CoinIcon} />
-            <span>남은 횟수 {userInfo.coin}</span>
-          </div>
-          <IoMdContact size={28} color="white" onClick={onNavigate} />
-        </Top>
-        <TextBox>
-          <span>코인은 매일 3개씩 지급돼요!</span>
-        </TextBox>
-        <RecomendBox>
-          <Text>
-            <span>새로운 친구들을 만나보세요!</span>
-          </Text>
-          <RecomendFriends>{showRecomend()}</RecomendFriends>
-        </RecomendBox>
-      </Header>
-      <Main>
-        <span>모든 대화방</span>
-        <ChatFriends>
-          {users.map((friend) => (
-            <ChatFriend
-              key={friend.id}
-              onClick={() =>
-                navigate(`/profile/${friend.id}`, {
-                  state: { userInfo, friend, users },
-                })
-              }
-            >
-              <ProfileImg
-                src={friend.gender === "female" ? FemaleImage : MaleImage}
-              />
-              <FriendInfo>
-                <span>{friend.name}</span>
-                <span>{friend.school}</span>
-              </FriendInfo>
-            </ChatFriend>
-          ))}
-        </ChatFriends>
-      </Main>
-    </HomeBackgroundBox>
+    <>
+      {rewordModal ? <Backdrop onClick={() => setRewordModal(false)} /> : null}
+      {rewordModal ? (
+        <Modal>
+          <span>출석 보상 코인 3개 지급 되었습니다!</span>
+          <img src={RobotSmileImage} />
+        </Modal>
+      ) : null}
+      <HomeBackgroundBox>
+        <Header>
+          <Top>
+            <div>
+              <CoinLogo src={CoinIcon} />
+              <span>남은 횟수 {userInfo.coin}</span>
+            </div>
+            <IoMdContact size={28} color="white" onClick={onNavigate} />
+          </Top>
+          <TextBox>
+            <span>코인은 매일 3개씩 지급돼요!</span>
+          </TextBox>
+          <RecomendBox>
+            <Text>
+              <span>새로운 친구들을 만나보세요!</span>
+            </Text>
+            <RecomendFriends>{showRecomend()}</RecomendFriends>
+          </RecomendBox>
+        </Header>
+        <Main>
+          <span>모든 대화방</span>
+          <ChatFriends>
+            {users.map((friend) => (
+              <ChatFriend
+                key={friend.id}
+                onClick={() =>
+                  navigate(`/profile/${friend.id}`, {
+                    state: { userInfo, friend, users },
+                  })
+                }
+              >
+                <ProfileImg
+                  src={friend.gender === "female" ? FemaleImage : MaleImage}
+                />
+                <FriendInfo>
+                  <span>{friend.name}</span>
+                  <span>{friend.school}</span>
+                </FriendInfo>
+              </ChatFriend>
+            ))}
+          </ChatFriends>
+        </Main>
+      </HomeBackgroundBox>
+    </>
   );
 }
 
